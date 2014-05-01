543097e197f9854c50356f61b681b19f6baf1d7b
Sven Tschui <sven.tschui@bluewin.ch>
2014-05-01 20:24:18 +0200

    fixed path to backbone.babysitter

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ require.config({
     'underscore.string': '../bower_components/underscore.string/lib/underscore.string',
     'jquery': '../bower_components/jquery/dist/jquery',
     'backbone': '../bower_components/backbone/backbone',
-    'backbone.babysitter': '../bower_components/backbone.babysitter/lib/amd/backbone.babysitter',
+    'backbone.babysitter': '../bower_components/backbone.babysitter/lib/backbone.babysitter',
     'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr',
     'backbone.localstorage': '../bower_components/backbone.localstorage/backbone.localStorage',
     'backbone.validation': '../bower_components/backbone-validation/dist/backbone-validation-amd',
