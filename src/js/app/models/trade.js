c064a74217df0d4555e5800d24116fa172bb6054
David Knezic <davidknezic@gmail.com>
2014-05-04 16:39:42 +0200

    Revamp new trade view, improve trade model

--- src/js/app/models/trade.js
+++ src/js/app/models/trade.js
@@ -3,7 +3,12 @@ define([
     'app/validations/trade',
     'moment',
     'underscore'
-  ], function (BaseModel, validation, moment, _) {
+  ], function (
+    BaseModel,
+    validation,
+    moment,
+    _
+  ) {
   return BaseModel.extend({
     validation: validation,
     defaults: {
@@ -36,12 +41,51 @@ define([
         if (!_.isNull(setValue) && !moment.isMoment(setValue))
           throw new Error('No moment object provided!');
 
-        return this.set('executionOn', newValue);
+        return this.set('executionOn', setValue.format());
       } else {
         // getter
         getValue = this.get('executionOn');
 
         return moment(getValue);
+      }
+    },
+
+    // not implemented yet
+    btcPrice: function () {
+      var setValue, getValue;
+
+      if (arguments.length > 0) {
+        // setter
+        setValue = arguments[0];
+      } else {
+        // getter
+        getValue = null;
+      }
+    },
+
+    // not implemented yet
+    amount: function () {
+      var setValue, getValue;
+
+      if (arguments.length > 0) {
+        // setter
+        setValue = arguments[0];
+      } else {
+        // getter
+        getValue = null;
+      }
+    },
+
+    // not implemented yet
+    fee: function () {
+      var setValue, getValue;
+
+      if (arguments.length > 0) {
+        // setter
+        setValue = arguments[0];
+      } else {
+        // getter
+        getValue = null;
       }
     }
   });
