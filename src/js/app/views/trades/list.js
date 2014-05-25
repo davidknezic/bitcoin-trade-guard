8100d60b53e67c23f988819fea4b85e04316a252
David Knezic <davidknezic@gmail.com>
2014-05-25 23:13:48 +0200

    Add show trade draft / selected labels collection

--- src/js/app/views/trades/list.js
+++ src/js/app/views/trades/list.js
@@ -2,17 +2,25 @@ define([
     'channel',
     'marionette',
     'app/templates/trades/list',
-    'app/views/trades/list-item'
+    'app/views/trades/list-item',
+    'app/views/labels/select',
+    'app/collections/labels'
   ], function (
     channel,
     Marionette,
     template,
-    TradesListItemView
+    TradesListItemView,
+    LabelSelectView,
+    LabelsCollection
   ) {
   return Marionette.CompositeView.extend({
     template: template,
     itemView: TradesListItemView,
     itemViewContainer: ".trades",
+
+    ui: {
+      labels: '.labels'
+    },
 
     events: {
       'click button.add-trade': 'addTrade'
@@ -22,6 +30,22 @@ define([
       'open': 'showTrade'
     },
 
+    initialize: function (options) {
+      this.labels = options.labels;
+      this.selectedLabels = new LabelsCollection();
+      this.selectedLabels.reset();
+    },
+
+    onShow: function () {
+      this.labelSelectView = new LabelSelectView({
+        el: this.ui.labels,
+        collection: this.labels,
+        selected: this.selectedLabels
+      });
+
+      this.labelSelectView.render();
+    },
+
     addTrade: function (event) {
       event.preventDefault();
 
@@ -29,8 +53,6 @@ define([
     },
 
     showTrade: function (eventName, view, model) {
-      event.preventDefault();
-
       channel.commands.execute('app:show:trade', model.cid);
     }
   });
--- src/js/app/views/trades/show.js
+++ src/js/app/views/trades/show.js
@@ -1,14 +1,38 @@
 define([
+    'channel',
     'marionette',
-    'app/templates/trades/show'
-  ], function (Marionette, template) {
+    'app/templates/trades/show',
+    'app/views/labels/select',
+    'app/collections/labels'
+  ], function (
+    channel,
+    Marionette,
+    template,
+    LabelSelectView,
+    LabelsCollection
+  ) {
   return Marionette.ItemView.extend({
     template: template,
+
+    ui: {
+      labels: '.labels'
+    },
 
     events: {
     },
 
     initialize: function (options) {
+      this.labels = channel.reqres.request('app:data:labels');
+    },
+
+    onShow: function () {
+      var labelsView = new LabelSelectView({
+        el: this.ui.labels,
+        collection: this.labels,
+        selected: new LabelsCollection() // temporary
+      });
+
+      labelsView.render();
     }
   });
 });
