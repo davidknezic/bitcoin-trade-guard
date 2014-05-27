cf787411b9d7454d6ac7cd52bceb0eb83a490939
David Knezic <davidknezic@gmail.com>
2014-05-27 23:25:07 +0200

    Implement trades list filtering / empty & loading

--- src/js/app/views/trades/list.js
+++ src/js/app/views/trades/list.js
@@ -1,16 +1,20 @@
 define([
+    'underscore',
     'channel',
     'marionette',
     'app/templates/trades/list',
     'app/views/trades/list-item',
     'app/views/labels/select',
+    'app/collections/trades',
     'app/collections/labels'
   ], function (
+    _,
     channel,
     Marionette,
     template,
     TradesListItemView,
     LabelSelectView,
+    TradesCollection,
     LabelsCollection
   ) {
   return Marionette.CompositeView.extend({
@@ -19,7 +23,10 @@ define([
     itemViewContainer: ".trades",
 
     ui: {
-      labels: '.labels'
+      labels: '.labels',
+      notEmpty: '.not-empty',
+      empty: '.empty',
+      loading: '.loading'
     },
 
     events: {
@@ -31,9 +38,15 @@ define([
     },
 
     initialize: function (options) {
+      this.trades = options.collection;
+      this.collection = new TradesCollection();
+      this.collection.reset(); // temporary
+
       this.labels = options.labels;
       this.selectedLabels = new LabelsCollection();
-      this.selectedLabels.reset();
+      this.selectedLabels.reset(); // temporary
+
+      this.selectedLabels.on('all', this.refilter, this);
     },
 
     onShow: function () {
@@ -44,6 +57,38 @@ define([
       });
 
       this.labelSelectView.render();
+
+      this.collection.on('all', this.onCollectionUpdate, this);
+
+      this.refilter();
+    },
+
+    onCollectionUpdate: function (eventName) {
+      if (eventName === 'request') {
+        return this.setState('loading');
+      }
+
+      if (this.collection.isEmpty()) {
+        return this.setState('empty');
+      } else {
+        return this.setState('not-empty');
+      }
+    },
+
+    refilter: function () {
+      var filtered = this.trades.models;
+
+      if (!this.selectedLabels.isEmpty()) {
+        var selectedLabels = this.selectedLabels.pluck('text');
+
+        filtered = filtered.filter(function (trade) {
+          return _.any(selectedLabels, function (label) {
+            return _.contains(trade.get('tags'), label);
+          });
+        });
+      }
+
+      this.collection.reset(filtered);
     },
 
     addTrade: function (event) {
@@ -54,6 +99,12 @@ define([
 
     showTrade: function (eventName, view, model) {
       channel.commands.execute('app:show:trade', model.cid);
+    },
+
+    setState: function (state) {
+      this.ui.notEmpty.toggle(state === 'not-empty');
+      this.ui.empty.toggle(state === 'empty');
+      this.ui.loading.toggle(state === 'loading');
     }
   });
 });
