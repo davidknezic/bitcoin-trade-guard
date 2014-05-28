f37f4f315e5ee39c8e69745e8dc27c93b4f23b74
David Knezic <davidknezic@gmail.com>
2014-05-28 23:22:39 +0200

    Convert settings view to layout

--- src/js/app/views/settings.js
+++ src/js/app/views/settings.js
@@ -2,13 +2,38 @@ define([
     'marionette',
     'app/templates/settings'
   ], function (Marionette, template) {
-  return Marionette.ItemView.extend({
+  return Marionette.Layout.extend({
     template: template,
 
+    regions: {
+      content: '.content'
+    },
+
+    ui: {
+      account: 'a.account',
+      sync: 'a.sync',
+      backup: 'a.backup'
+    },
+
     events: {
+      'click @ui.account': 'clickAccount',
+      'click @ui.sync': 'clickSync',
+      'click @ui.backup': 'clickBackup'
     },
 
     initialize: function (options) {
+    },
+
+    clickAccount: function (event) {
+      event.preventDefault();
+    },
+
+    clickSync: function (event) {
+      event.preventDefault();
+    },
+
+    clickBackup: function (event) {
+      event.preventDefault();
     }
   });
 });
