ae0209ced4f39a0bdabf369944dd5a52549f3af9
David Knezic <davidknezic@gmail.com>
2014-05-31 02:38:41 +0200

    Add functionality to `bitstamp` module def

--- src/js/bitstamp/module.js
+++ src/js/bitstamp/module.js
@@ -1,15 +1,36 @@
 define([
     'channel',
-    'marionette'
-  ], function (channel, Marionette) {
+    'marionette',
+    'app/models/service',
+    'bitstamp/views/new',
+    'bitstamp/views/edit'
+  ], function (channel, Marionette, Service, NewView, EditView) {
   return Marionette.Module.extend({
-    initialize: function(options, moduleName, app) {
+    initialize: function (options, moduleName, app) {
     },
 
-    onStart: function(options) {
+    onStart: function (options) {
+      this.services = channel.reqres.request('app:data:services');
+
+      this.service = this.services.add({
+        name: 'Bitstamp',
+        slug: 'bitstamp'
+      });
+
+      channel.reqres.setHandler('bitstamp:view:new', function () {
+        return new NewView();
+      });
+
+      channel.reqres.setHandler('bitstamp:view:edit', function (sync) {
+        return new EditView();
+      });
     },
 
-    onStop: function(options) {
+    onStop: function (options) {
+      this.services.remove(this.service);
+
+      channel.reqres.removeHandler('bitstamp:view:new');
+      channel.reqres.removeHandler('bitstamp:view:edit');
     },
   });
 });
