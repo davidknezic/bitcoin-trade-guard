b8bc7714b41e136d576b48164f4e024216f425d9
David Knezic <davidknezic@gmail.com>
2014-06-01 23:50:39 +0200

    Add clientId field to Bitstamp new view

--- src/js/bitstamp/views/new.js
+++ src/js/bitstamp/views/new.js
@@ -8,6 +8,7 @@ define([
 
     ui: {
       title: '.title',
+      clientId: '.client-id',
       key: '.key',
       secret: '.secret',
       submit: 'button[type="submit"]'
@@ -27,6 +28,8 @@ define([
     },
 
     setLoading: function (isLoading) {
+      this.ui.title.prop('disabled', isLoading);
+      this.ui.clientId.prop('disabled', isLoading);
       this.ui.key.prop('disabled', isLoading);
       this.ui.secret.prop('disabled', isLoading);
       this.ui.submit.button(isLoading ? 'loading' : 'reset');
@@ -37,6 +40,7 @@ define([
         service: 'bitstamp',
         title: this.ui.title.val(),
         data: {
+          clientId: this.ui.clientId.val(),
           key: this.ui.key.val(),
           secret: this.ui.secret.val()
         }
